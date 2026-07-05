b6713d20082257c7a6db03c573ce27d19feae8a1
Mamoru-Yuasa <s2521145@u.tsukuba.ac.jp>
2026-07-05 18:40:06 +0900

    fix: update version number to 1.1.1 in setup.py and add WRF_tools dependency in environment.yml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wrf_tools',
-    version='0.1.0',
+    version='1.1.1',
     author='Quang-Van Doan',
     author_email='doan.van.gb@...',
     description='Tools for processing WRF and downloading ERA5 data',
